114b9d4cec85a0279c7021b2075f3fc50ae4a0c3
owsibowsi <49979155+owsibowsi@users.noreply.github.com>
2020-05-10 14:25:52 +0200

    Add files via upload

---
+++ precache-manifest.1d0af9e810402fc71ce29b77a16f3da8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f50e14e682dd399dd0",
+    "url": "/css/app.053ed521.css"
+  },
+  {
+    "revision": "47fba0ef74263f6f1adb",
+    "url": "/css/chunk-04a4bd12.84532ea2.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "979e9df66c553560640f0ccf5e8b01e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5f50e14e682dd399dd0",
+    "url": "/js/app.30d5f661.js"
+  },
+  {
+    "revision": "47fba0ef74263f6f1adb",
+    "url": "/js/chunk-04a4bd12.b791f04b.js"
+  },
+  {
+    "revision": "82beae6038334dfc051b",
+    "url": "/js/chunk-vendors.146c2947.js"
+  },
+  {
+    "revision": "dfb63227c051fa0759d16f3a20869835",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.1d0af9e810402fc71ce29b77a16f3da8.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vuefirebase"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
